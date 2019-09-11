21d6416a21041be861f2cd83befaeb81528ca904
Michail <zamotin-mihail@mail.ru>
2019-09-11 21:47:23 +0300

    добавил необходимые файлы в новый репозиторий в соответствии с заданием

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Your language")
+
+
+@pytest.fixture(scope="function")
+def browser(request, language='en'):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(8)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
