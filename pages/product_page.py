d8b96a9e9ca17d47b83935bfa331ef62fd93b0c9
Michail <zamotin-mihail@mail.ru>
2019-11-09 15:42:58 +0300

    replaced few methods from product_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -25,15 +25,3 @@ class ProductPage(BasePage):
 
     def should_be_disappeared(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message don't disappeared"
-
-    def should_be_empty_basket_message(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), \
-            'No empty basket message, but it should'
-
-    def should_be_product_in_basket(self):
-        assert self.is_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), \
-            'No products in basket'
-
-    def should_not_be_products_in_basket(self):
-        assert not self.is_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), \
-            "Products in basket, but shouldn't"
